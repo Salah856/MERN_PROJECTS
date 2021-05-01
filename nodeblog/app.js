f65f859bf247e054c56eacd3dc0cfad0ff23cb69
Salah Othman <salah_elhossiny856@yahoo.com>
2021-05-01 14:43:43 +0200

    Update app.js

--- nodeblog/app.js
+++ nodeblog/app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
+
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var multer = require('multer');
