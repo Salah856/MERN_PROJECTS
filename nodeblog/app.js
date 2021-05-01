732982ed3cef7465a9cc791c59a9b14b03bf4740
Salah Othman <salah_elhossiny856@yahoo.com>
2021-05-01 14:43:31 +0200

    Update app.js

--- nodeblog/app.js
+++ nodeblog/app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var multer = require('multer');
+
 var upload = multer({ dest: 'uploads/' })
 
 var expressValidator = require('express-validator');
