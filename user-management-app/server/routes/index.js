a1b7dda8c5d7871f280c7b82e72511375c117dc1
Salah Othman <salah_elhossiny856@yahoo.com>
2021-05-10 09:55:10 +0200

    Update index.js

--- user-management-app/server/routes/index.js
+++ user-management-app/server/routes/index.js
@@ -1,20 +1,10 @@
-/*
- * File: index.js
- * Project: user-management
- * File Created: Wednesday, 5th June 2019 12:51:27 am
- * Author: Matthew Raymer (matthew.raymer@anomalistdesign.com)
- * -----
- * Last Modified: Wednesday, 5th June 2019 12:46:50 pm
- * Modified By: Matthew Raymer (matthew.raymer@anomalistdesign.com>)
- * -----
- * Copyright 2019, ADDLC
- */
-
 const fs = require('fs');
 
 module.exports = (app) => {
+  
   // API routes
   fs.readdirSync(__dirname + '/api/').forEach((file) => {
     require(`./api/${file.substr(0, file.indexOf('.'))}`)(app);
   });
 };
+
