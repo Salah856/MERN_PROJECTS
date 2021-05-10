488c68963f0381ee69c4c516ca2d4edb3785ce4a
Salah Othman <salah_elhossiny856@yahoo.com>
2021-05-10 09:52:31 +0200

    Update helpers.js

--- user-management-app/config/helpers.js
+++ user-management-app/config/helpers.js
@@ -1,6 +1,8 @@
 const path = require('path');
 
+
 // Helper functions
+
 function root(args) {
   args = Array.prototype.slice.call(arguments, 0);
   return path.join.apply(path, [__dirname].concat('../', ...args));
