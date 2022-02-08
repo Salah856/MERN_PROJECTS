c76ef30d53ded941ac288b2b77d56ff65e50babe
Salah Elhossiny <salah_elhossiny856@yahoo.com>
2022-02-08 13:09:16 +0200

    Create time.js

---
+++ time.js
@@ -0,0 +1,11 @@
+
+function date(d){
+  
+  return new Date(d).toLocaleDateString("en-US", {
+            year: 'numeric', 
+            month: 'long', 
+            day: 'numeric'
+        }); 
+
+
+}
