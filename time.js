4db582dbf4fa7854d1848914d34d8a02f5f5e6b6
Salah Elhossiny <salah_elhossiny856@yahoo.com>
2022-02-08 13:11:11 +0200

    Update time.js

--- time.js
+++ time.js
@@ -1,11 +1,8 @@
 
 function date(d){
-  
   return new Date(d).toLocaleDateString("en-US", {
                   year: 'numeric', 
                   month: 'long', 
                   day: 'numeric'
               }); 
-
-
 }
