236a72f2f4dcc88e5874f4153d9f2e3bdd69e7ee
Salah Elhossiny <salah_elhossiny856@yahoo.com>
2022-04-01 10:10:44 +0200

    Create map.js

---
+++ map.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import GoogleMapReact from 'google-map-react'
+
+
+const location = {
+  address: '1600 Amphitheatre Parkway, Mountain View, california.',
+  lat: 37.42216,
+  lng: -122.08427,
+} 
+
+
+const Map = ({ location, zoomLevel=15 }) => (
+  
+  
+  <div className="map">
+    <h2 className="map-h2">Come Visit Us At Our Campus</h2>
+
+    <div className="google-map">
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: '' }}
+        defaultCenter={location}
+        defaultZoom={zoomLevel}
+      >
+      </GoogleMapReact>
+    </div>
+  </div>
+)
+
